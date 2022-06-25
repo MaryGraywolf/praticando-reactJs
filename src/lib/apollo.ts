0df1362351449433a0fac987eaeca3c2ced23dc3
MaryGraywolf <marysantoslima24@gmail.com>
2022-06-25 14:44:53 -0300

    Update of Router and Lesson and, creation of Subscribe and Assets

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rbmdwd0v9w01z519l3bqh7/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
